f3f1fdcddfc33293d06d25687b6ec75a0792803e
Xikui Wang <xkkwww@gmail.com>
2016-03-30 16:13:47 -0700

    Add asterixOBJ

--- src/main/java/edu/mongo2asterix/sys/MongoCollection2AsterixDataset.java
+++ src/main/java/edu/mongo2asterix/sys/MongoCollection2AsterixDataset.java
@@ -1,7 +1,10 @@
 package edu.mongo2asterix.sys;
 
 import com.mongodb.Mongo;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import edu.mongo2asterix.asterix.AsterixDatabase;
+import org.bson.Document;
 
 /**
  * Created by Xikui on 3/30/16.
@@ -17,7 +20,17 @@ public class MongoCollection2AsterixDataset {
     }
 
     public void transfer(MongoDatabase mdb, AsterixDatabase adb, String tableName){
+        // for record in mdb tablename insert into adb
+        MongoCollection<Document> mongoCollection = mdb.getCollection(tableName);
+        for (Document doc : mongoCollection.find()){
+            AsterixObj aobj = new AsterixObj();
+            for (String attrName : doc.keySet()){
+                Object keyObj = doc.get(attrName);
+                if(keyObj instanceof String){
 
+                }
+            }
+        }
     }
 
 }
