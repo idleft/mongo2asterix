0b891c275dc0ecde08e4c6a095111468b9daad9b
Xikui Wang <xkkwww@gmail.com>
2016-03-31 15:06:14 -0700

    Change ADBHelper to non-static

--- src/main/java/edu/mongo2asterix/asterix/AsterixDBHelper.java
+++ src/main/java/edu/mongo2asterix/asterix/AsterixDBHelper.java
@@ -16,10 +16,10 @@ import java.util.List;
  */
 public class AsterixDBHelper {
 
-//    public static AsterixDBHelper instance;
-    public static String asterixHost;
-    public static Integer asterixPort;
-    private static String asterixURL;
+    //    public static AsterixDBHelper instance;
+    public String asterixHost;
+    public Integer asterixPort;
+    private String asterixURL;
 
     // Might not be necessary to use singleton
 //    public static AsterixDBHelper getInstance(){
@@ -28,20 +28,20 @@ public class AsterixDBHelper {
 //        return instance;
 //    }
 
-    public static void InitAsterixDBHelper(String asterix_host, Integer asterix_port){
+    public void AsterixDBHelper(String asterix_host, Integer asterix_port) {
         asterixHost = asterix_host;
         asterixPort = asterix_port;
-        asterixURL = "http://"+asterixHost+":"+String.valueOf(asterixPort);
+        asterixURL = "http://" + asterixHost + ":" + String.valueOf(asterixPort);
     }
 
-    public static int putRequest(String requestType, String aql)  {
+    public int putRequest(String requestType, String aql) {
         URL requestURL = null;
         HttpURLConnection conn = null;
         try {
             requestURL = new URL(asterixURL);
             conn = (HttpURLConnection) requestURL.openConnection();
             conn.setRequestMethod("GET");
-            conn.setRequestProperty(requestType,aql);
+            conn.setRequestProperty(requestType, aql);
             return conn.getResponseCode();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -54,30 +54,30 @@ public class AsterixDBHelper {
     }
 
 
-    public static void createDataverse(String dvName){
-        String requestAQL = String.format("drop dataverse %s if exists; create dataverse %s",dvName,dvName);
-        putRequest("ddl",requestAQL);
+    public void createDataverse(String dvName) {
+        String requestAQL = String.format("drop dataverse %s if exists; create dataverse %s", dvName, dvName);
+        putRequest("ddl", requestAQL);
     }
 
-    public static void createDataType(String dvName, String dtName, HashMap<String, String> dataTypes){
+    public void createDataType(String dvName, String dtName, HashMap<String, String> dataTypes) {
         String aqlTemplate = "use dataverse %s; create type %s as open{ %s}";
         List<String> dataTypeList = new ArrayList();
-        for (String attrName:dataTypes.keySet()){
+        for (String attrName : dataTypes.keySet()) {
             String attrType = dataTypes.get(attrName);
-            dataTypeList.add(attrName+":"+attrType);
+            dataTypeList.add(attrName + ":" + attrType);
         }
-        String requestAQL = String.format(aqlTemplate,dvName,dtName,String.join(",",dataTypeList));
-        putRequest("ddl",requestAQL);
+        String requestAQL = String.format(aqlTemplate, dvName, dtName, String.join(",", dataTypeList));
+        putRequest("ddl", requestAQL);
     }
 
-    public static String castObjectToAsterixString(Object obj){
+    public String castObjectToAsterixString(Object obj) {
         return "";
     }
 
-    public static void createDataSet(String dvName, String dsName, String dtName, String pkeyName){
+    public void createDataSet(String dvName, String dsName, String dtName, String pkeyName) {
         String aqlTemplate = "use dataverse %s; create dataset %s(%s) primary key %s;";
-        String requestAQL = String.format(aqlTemplate,dvName,dsName,dtName,pkeyName);
-        putRequest("ddl",requestAQL);
+        String requestAQL = String.format(aqlTemplate, dvName, dsName, dtName, pkeyName);
+        putRequest("ddl", requestAQL);
     }
 
 }
